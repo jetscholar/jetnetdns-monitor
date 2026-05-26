aae9ac5ca1b2fc172f607c2e55b1c8b11d0b6dec
jetscholar <mrz@jetscholar.com>
2026-05-26 18:03:50 +1000

    gui updates2

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+import os
+
 from dotenv import load_dotenv
+from flask import Flask, render_template
+
 from routes.status_routes import status_bp
 
 
@@ -20,9 +23,13 @@ app = create_app()
 
 
 if __name__ == "__main__":
+	host = os.getenv("APP_HOST", "127.0.0.1")
+	port = int(os.getenv("APP_PORT", "3007"))
+	debug = os.getenv("APP_ENV", "development") == "development"
+
 	app.run(
-		host="127.0.0.1",
-		port=3007,
-		debug=True,
+		host=host,
+		port=port,
+		debug=debug,
 		use_reloader=False
 	)
